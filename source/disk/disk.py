8ecb1ed02e504f367ffc283ed84f6e212c31ac89
pubnikname99 <work-nik@fedora-lt1>
2024-03-13 13:30:02 +0200

    Added functionality with python to extract disk usage data and automatically generate a disk usage template

---
+++ source/disk/disk.py
@@ -0,0 +1,58 @@
+# Imports
+from re import search
+from sys import argv
+from os import path
+
+# Variables and lists:
+disk_spaces = [80, 160, 320, 640, 1280, 1920]
+df_info = argv[1].replace("Mounted on", "Mounted on\n") # df info
+df_only_total_size = argv[2] # Only server size from df
+df_only_total_size = int(df_only_total_size.replace('G', '')) # Remove G from server size
+root_dirs = argv[3] # Largest directories in /
+backup_dirs = argv[4] # All backup directories
+large_files_found = argv[5] # Largest files on the server
+jb_status = argv[6] # Detect if JB is present
+admin_name = argv[7] # The admin name that will appear in the ticket signature
+my_path = path.abspath(path.dirname(__file__))
+
+# Retrieve the mini templates:
+with (
+    open(path.join(my_path, "templates/handle_if_backups.txt")) as file1,
+    open(path.join(my_path, "templates/if_large_files_found.txt")) as file2,
+    open(path.join(my_path, "templates/upgrade_recommend.txt")) as file3,
+    open(path.join(my_path, "templates/jb_template.txt")) as file4
+):
+    handle_if_backups = file1.read()
+    if_large_files_found = file2.read()
+    upgrade_recommend = file3.read()
+    upgrade_recommend = upgrade_recommend.replace("[current_space]", f"{df_only_total_size}G")
+    upgrade_disk_space = disk_spaces.index(min(disk_spaces, key=lambda x:abs(x-df_only_total_size))) +1
+    upgrade_recommend = upgrade_recommend.replace("[upgrade_server]", f"{disk_spaces[upgrade_disk_space]}G")
+    jb_template = file4.read()
+
+# Retrieve the main template and edit it accordingly:
+with open(path.join(my_path, "templates/template.txt")) as file:
+    chosen_template = file.read().strip()
+    complete_template = chosen_template.replace("[df_info]", df_info)
+    complete_template = complete_template.replace("[root_dirs]", root_dirs)
+    # If there is a /backup dir found on the server, add it to the template and then combine templates
+    if search("backup", root_dirs):
+        handle_if_backups = handle_if_backups.replace("[backup_dirs]", backup_dirs)
+        complete_template = complete_template.replace("[handle_if_backups]", handle_if_backups)
+    else:
+        complete_template = complete_template.replace("[handle_if_backups]", "")
+    # If there are large files found on the server, add them to the template and then combine templates
+    if large_files_found != "":
+        if_large_files_found = if_large_files_found.replace("[large_files_found]", large_files_found)
+        complete_template = complete_template.replace("[if_files_found]", if_large_files_found)
+    else:
+        complete_template = complete_template.replace("[if_files_found]", "")
+    complete_template = complete_template.replace("[upgrade_recommend]", upgrade_recommend)
+    # If JetBackup 4/5 is found on the server add the template or empty space in its place:
+    if jb_status == "jb":
+        complete_template = complete_template.replace("[jb_template]", "")
+    else:
+        complete_template = complete_template.replace("[jb_template]", jb_template)
+    complete_template = complete_template.replace("[admin_name]", admin_name)
+
+print(complete_template)
--- source/disk/disk.sh
+++ source/disk/disk.sh
@@ -1,3 +1,22 @@
 #! /bin/bash
 
-echo "empty words"
+# This part of the script is designed to extract data only and pass it along
+# to the Python script that will handle the template building.
+
+# Retrieve the information that we normally do when performing disk breakdown:
+read -r -d '' -a df_info < <( df -BG -h / )
+root_dirs="$(du -sh --exclude='/home/virtfs/*' /* | grep G | sort -rh)"
+backup_dirs="$(du -sh /backup/* | grep G | sort -rh && du -sh /backup/weekly/* | grep G | sort -rh)"
+large_files_found="$(find /home -maxdepth 7 -type f -size +200M -exec du -hsxc {} + | sort -rh | grep -v total && find /backup -maxdepth 7 -type f -size +200M -exec du -hsxc {} + | sort -rh | grep -v total)"
+
+# Check if JB4/5 are installed on the server and capture them in variables:
+if ( jetbackup --version || jetbackup5 --version ) >/dev/null 2>&1
+then
+        jb_status="jb"
+else
+        jb_status=""
+fi
+
+# Execute python script and pass along the info as variables:
+python "$DIR/source/disk/disk.py" "${df_info[*]}" "${df_info[8]}" "$root_dirs" "$backup_dirs" "$large_files_found" "$jb_status" "$admin_name"
+
