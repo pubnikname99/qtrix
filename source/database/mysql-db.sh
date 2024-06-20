e9135dea5f86810ef4d6d434585325b520e5d22e
pubnikname99 <work-nik@fedora-lt1>
2024-06-20 03:55:49 +0300

    Minor bug fixes, aesthetic changes

--- source/database/mysql-db.sh
+++ source/database/mysql-db.sh
@@ -9,23 +9,22 @@ echo -e "\n${YELLOW}--- DATABASE RELATED ---${ENDCOLOR}"
 echo -e "${L_BLUE}- MySQL version:${ENDCOLOR}"
 mysql --version
 
-echo -e "${L_BLUE}- Current processlist and number of rows below it::${ENDCOLOR}"
+echo -e "\n${L_BLUE}- Current processlist and number of rows below it::${ENDCOLOR}"
 mysqladmin processlist
-num_of_rows="$(mysqladmin processlist | wc -l)"
+num_of_rows="$(($(mysqladmin processlist | wc -l) - 4))"
 echo -e "${YELLOW} Total rows: $num_of_rows${ENDCOLOR}"
 
-echo -e "${L_BLUE}- Users performing slow MySQL queries today:${ENDCOLOR}"
+echo -e "\n${L_BLUE}- Users performing slow MySQL queries today (if any):${ENDCOLOR}"
 read -r -d '' -a cur_slow_cnf < <( grep slow_query_log /etc/my.cnf | cut -d '=' -f 2 )
 
-if [[ ${cur_slow_cnf[0]} == "1" ]]
+if [[ ${cur_slow_cnf[0]} -eq 1 ]]
 then
         # Create the variable holding today's date in the MySQL slow query log format
         cur_time="$(date '+%Y-%m-%d')"
-        
-        # Retrieve the number of requests for the desired time for a domain:
+        # Retrieve the number of users and the number of slow queries performed:
         grep -a1 $cur_time /var/lib/mysql/mysql_slow.log | grep 'User@Host' | cut -d '[' -f 2 | cut -d ']' -f 1 | sort | uniq -c | sort -rh
 else
-        echo "Slow query logging appears to be disabled."
+        echo "${RED}Slow query logging appears to be disabled.${ENDCOLOR}"
 fi
 
 
